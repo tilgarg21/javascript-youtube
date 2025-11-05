c8c91672fe01d7dc2937dfe546b2cce4dd0e092a
Tilak Garg <tilgarg21@gmail.com>
2025-11-05 13:09:41 +0530

    comparisons and data types summary

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,32 @@
+// Primitive
+// 7 types: String, Number, Boolean, null , undefined, Symbol, BigInt
+
+const score = 100;
+const scorevalue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+// console.log(id === anotherId);
+
+const bigNumber = 156418451897998n;
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "Tilak",
+    age: 23
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+
